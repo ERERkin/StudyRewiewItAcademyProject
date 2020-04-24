beea7cd6cb9459e64d3ead610080744f277eafd0
ERERkin <erkin1399@gmail.com>
2020-04-24 18:49:40 +0600

    Create Project

---
+++ src/main/java/it/academy/StudyRewiewItAcademyProject/StudyRewiewItAcademyProjectApplication.java
@@ -0,0 +1,13 @@
+package it.academy.StudyRewiewItAcademyProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRewiewItAcademyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyRewiewItAcademyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/academy/StudyRewiewItAcademyProject/StudyRewiewItAcademyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.academy.StudyRewiewItAcademyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRewiewItAcademyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
