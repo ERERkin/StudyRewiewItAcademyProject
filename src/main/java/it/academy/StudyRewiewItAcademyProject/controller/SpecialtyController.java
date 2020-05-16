192fc53c889f3c07717bc4b190b62ac4b4dbf3e2
ERERkin <erkin1399@gmail.com>
2020-05-16 14:07:22 +0600

    Added controllers in other branch

--- src/main/java/it/academy/StudyRewiewItAcademyProject/controller/SpecialtyController.java
+++ src/main/java/it/academy/StudyRewiewItAcademyProject/controller/SpecialtyController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/comment")
-public class CommentController {
+@RequestMapping("/specialty")
+public class SpecialtyController {
 }
--- src/main/java/it/academy/StudyRewiewItAcademyProject/controller/UniversityController.java
+++ src/main/java/it/academy/StudyRewiewItAcademyProject/controller/UniversityController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/comment")
-public class CommentController {
+@RequestMapping("/university")
+public class UniversityController {
 }
